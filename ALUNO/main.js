467b4fc8c7460441d99273a238155b108d0fb7e9
Bramsouza <bruna.alvesmsouza@gmail.com>
2023-09-03 23:10:45 -0300

    Add files via upload

---
+++ ALUNO/main.js
@@ -0,0 +1,77 @@
+//o FABRIC.JS é uma extensão de códigos para a criação de jogos
+//aqui o canvas recebe a referência com fabric.Canvas() 
+
+
+//variável utilizada para armazenar o objeto da imagem do personagem.
+
+
+//definir a width e height iniciais das imagens dos blocos
+
+
+//definição das coordenadas x e y iniciais para o personagem
+
+
+//variável será utilizada para armazenar o objeto da imagem do personagem
+
+
+//Agora, adicionaremos uma função chamada playerUpdate() para adicionar a imagem do personagem
+function playerUpdate(){
+	//lembrando que o fabric é da biblioteca que foi chamada no html 
+	//o image diz que enviamos uma imagem.
+	//fromURL contém a URL da imagem e a função para enviar imagens.
+	//o player.png é a imagem que será carregada 
+	//o  function(Img) é a função que enviará player.png ao canvas. Img: Esse é o objeto da imagem definida por padrão.
+	
+
+	//sempre que deletar, adicionar, modificar a width e height da imagem do jogador, referimos a variável playerObject
+	
+
+	//playerObject é o objeto da imagem. podemos alterar a largura e altura com os scales's
+	
+
+	//***AJUSTE DE COORDENADAS***//
+	//playerObject é o objeto da imagem.
+	//○ set ajustaremos as coordenadas x e y da imagem do personagem.
+	
+		//○ top representa a coordenada y.
+		
+		//left representa a coordenada x.
+
+
+	//canvas é a variável criada no início, que contém o elemento canvas.
+	//○ add(): é a função para adicionar os desenhos ao canvas.
+	//playerObject É o objeto imagem do personagem.
+	
+}
+//função para adicionar as diferentes imagens de acordo com as teclas específicas pressionadas.
+//getImage. Essa variável conterá as respectivas imagens dos blocos de acordo com as teclas pressionadas
+function newImage(getImage){
+
+	//fabric: nome da biblioteca que utilizamos.
+	//Image: diz que estamos enviando uma imagem.
+	//fromURL: Esse conterá a URL da imagem e a função de enviar uma imagem.
+	//getImage: Essa variável conterá as respectivas imagens das teclas específicas que foram pressionadas.
+	//function(Img): Essa é a função que enviará player.png ao canvas. ■ Img: Esse é o objeto da imagem ajustado por padrão.
+	
+
+	//armazenando o objeto padrão da imagem na variável.
+	
+
+	//blockImageObject é o objeto da imagem
+	//scaleToWidth(blockImageWidth) e scaleToHeight(blockImageHeight)  ajustará a Height da imagem.
+	//blockImageWidth: Ajustamos a width para o valor blockImageWidth, que é 30, já definido anteriormente.
+	
+
+	//blockImageObject é o objeto da imagem.
+	//set ajustará as coordenadas x e y da imagem do personagem.
+	//top representa a coordenada y.
+	//left representa a coordenada x.
+	
+
+	//canvas é a variável criada no início que contém o elemento canvas.
+	//add(blockImageHeight): Essa é a função para adicionar qualquer coisa ao canvas.
+	//blockImageHeight é o objeto da nova imagem.
+
+
+	
+}
